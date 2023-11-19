c96c0512fd33d14bbe73b03bfe90d2916f052c3d
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-11-19 14:05:50 +0600

    Add lesson 07-operators

---
+++ 07-operators/script.js
@@ -0,0 +1,49 @@
+// Operators
+
+// Arithmetic operators
+let x = 5;
+let y = 5;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+// contatenattion
+let first = "john";
+let last = "doe";
+
+console.log(first + last);
+
+// increment
+let z = 1;
+// console.log(z++);
+console.log(++z);
+
+// decrement
+console.log(z--);
+console.log(--z);
+
+// assignment
+let d = 10;
+d += 10;
+d -= 2;
+d *= 3;
+d /= 2;
+d %= 2;
+d **= 3;
+
+// comparison
+x = 4;
+y = 3;
+console.log(x > y);
+console.log(x >= y);
+console.log(x < y);
+console.log(x <= y);
+console.log(x == y);
+console.log(x === y);
+console.log(x != y);
+console.log(x !== y);
+console.log(x !== y);
