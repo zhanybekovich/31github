a63acf529851a2e79f4862c1b842fbb546d2d28b
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-11-19 14:49:28 +0600

    Add lesson 10-work-with-nums

---
+++ 10-working-with-numbers/script.js
@@ -0,0 +1,15 @@
+// working with numbers
+let num = 5;
+
+// number to string
+console.log(num.toString());
+
+// getting how many digits
+console.log(num.toString().length);
+
+// to specify number decimals after point
+console.log(num.toFixed(2));
+
+console.log(num.toPrecision(2));
+console.log(num.toExponential(2));
+console.log(num.toLocaleString("ar-EG"));
