914a2f54d834e2994481a07954e9ecb58165f3d7
= <=>
2024-01-02 15:13:25 +0600

    Add lesson 65-ui-check feature

---
+++ JS-practice/65-shop-list-not-display-filter/shopping-list/script.js
@@ -0,0 +1,87 @@
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const clear = document.getElementById("clear");
+
+const itemFilter = document.getElementById("filter");
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+
+  return icon;
+}
+
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+
+  const icon = createIcon("fa-solid fa-xmark");
+
+  button.appendChild(icon);
+
+  return button;
+}
+
+function addItem(e) {
+  e.preventDefault();
+
+  const newItem = itemInput.value;
+
+  // Validate input
+  if (newItem === "") {
+    alert("Add Item");
+    return;
+  }
+
+  // create new element
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+
+  checkUI();
+
+  itemInput.value = "";
+}
+
+function removeItem(e) {
+  // console.log(e.target);
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    e.target.parentElement.parentElement.remove();
+
+    checkUI();
+  }
+}
+
+function clearItems() {
+  // itemList.innerHTML = "";
+
+  // better way
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+
+  checkUI();
+}
+
+//
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+
+  if (items.length === 0) {
+    clear.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clear.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clear.addEventListener("click", clearItems);
+checkUI();
