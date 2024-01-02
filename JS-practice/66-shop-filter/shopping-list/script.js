327fd9da073b6fc8065e01eff4c62aa6fd2749f5
= <=>
2024-01-02 15:43:04 +0600

    Add lesson 66-filter feature

---
+++ JS-practice/66-shop-filter/shopping-list/script.js
@@ -0,0 +1,103 @@
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const clear = document.getElementById("clear");
+
+const itemFilter = document.getElementById("filter");
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+
+  return icon;
+}
+
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+
+  const icon = createIcon("fa-solid fa-xmark");
+
+  button.appendChild(icon);
+
+  return button;
+}
+
+function addItem(e) {
+  e.preventDefault();
+
+  const newItem = itemInput.value;
+
+  // Validate input
+  if (newItem === "") {
+    alert("Add Item");
+    return;
+  }
+
+  // create new element
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+
+  checkUI();
+
+  itemInput.value = "";
+}
+
+function removeItem(e) {
+  // console.log(e.target);
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    e.target.parentElement.parentElement.remove();
+
+    checkUI();
+  }
+}
+
+function clearItems() {
+  // itemList.innerHTML = "";
+
+  // better way
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+
+  checkUI();
+}
+
+//
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+
+  if (items.length === 0) {
+    clear.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clear.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
+function filterItems(e) {
+  const text = e.target.value.toLowerCase();
+
+  const items = itemList.querySelectorAll("li");
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clear.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
+checkUI();
