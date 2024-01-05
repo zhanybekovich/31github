56cd1db8c52057b7eee43476c43de5f9017f256d
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-01-05 18:44:39 +0600

    Add solution 510a

---
+++ Codeforce-python/510a.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+
+for i in range(n):
+    if i % 2 == 0:
+        print('#' * m)
+    elif (i + 1) % 4 == 0:
+        print('#' + '.' * (m - 1))
+    else:
+        print('.' * (m - 1) + '#')
