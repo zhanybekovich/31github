1a6be64d57090893f2ca5c16f17edae06208b089
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-01-17 21:32:23 +0600

    Usage of geo location on leaflet map

---
+++ JS-practice/03-web-api/02-map-position/script.js
@@ -0,0 +1,18 @@
+const map = L.map("map").setView([0, 0], 2);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+const marker = L.marker([0, 0]).addTo(map);
+
+navigator.geolocation.getCurrentPosition(function (pos) {
+  const lat = pos.coords.latitude;
+  const lng = pos.coords.longitude;
+
+  marker.setLatLng([lat, lng]).update();
+  map.setView([lat, lng], 15);
+
+  marker.bindPopup("<strong>Hello</strong><br>This is my location");
+});
