5d2115c3b8f351d854b386e20aa3b177844d4cee
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-01-21 13:14:00 +0600

    Built in constructors

---
+++ JS-practice/06-oop/02-literals-vs-constructors/script.js
@@ -0,0 +1,17 @@
+const strLit = "Hello";
+const strObj = new String("Hello 2");
+
+console.log(strLit);
+console.log(strObj);
+
+const x = 15;
+const y = new Number(20);
+
+console.log(x);
+console.log(y);
+
+const yes = true;
+const no = new Boolean(false);
+
+console.log(yes);
+console.log(no);
