7df47731d4c4b69a7d0dc13d8c90d8f2feac36cb
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-01-21 13:25:19 +0600

    Prototype

---
+++ JS-practice/06-oop/03-prototypes/script.js
@@ -0,0 +1,17 @@
+function Rectangle(name, width, height) {
+  this.name = name;
+  this.width = width;
+  this.height = height;
+  this.area = function () {
+    return this.width * this.height;
+  };
+}
+
+const rect = new Rectangle("Rect", 10, 10);
+
+//  Rectangle.prototype inherits from Object.prototype. That's why we can use toString(), etc
+console.log(rect);
+console.log(rect.toString());
+
+// To get the prototype of an object
+console.log(Object.getPrototypeOf(rect));
